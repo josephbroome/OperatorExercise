93d8cf394ad729901338e8f0ccc46bf370f788d1
Joseph Broome <josephlloydbroome@gmail.com>
2022-06-06 13:04:00 -0700

    updates

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        var a = 17;
+        var b = 4;
+
+        var quotient = a / b;
+        var remainder = a % b;
+
+
+
+        Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+        AreaOfCircle();
+
+        
+
+    }
+  public static void AreaOfCircle()
+    {
+        Console.WriteLine("What is the radius of your circle");
+
+
+        var radius = double.Parse(Console.ReadLine());
+
+        var pi= Math.PI;
+
+       var AreaofCircle= pi*(radius * radius);
+
+        Console.WriteLine($" The area of a cirlce with a radius of {radius} is {AreaofCircle}");
+
+
+
+    }
+
+}
